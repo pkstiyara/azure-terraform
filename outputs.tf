a67d45eee480a4e8426c86859701caedae028e96
admin <pkstiyara+dockerbb@gmail.com>
2023-11-02 20:07:25 +0530

    aks_cluster_setup

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,7 @@ output "ip" {
     description = "vm_ip"
     value = azurerm_linux_virtual_machine.main_vm.public_ip_address
 }
+
+output "key_data" {
+  value = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
+}

2a2379f606e76d2b373296408da87285e9d9b58a
admin <pkstiyara+dockerbb@gmail.com>
2023-12-18 07:18:04 +0530

    jenkins-node-updated

--- main.tf
+++ main.tf
@@ -95,43 +95,43 @@ data "azurerm_public_ip" "vm_ip" {
 ##################################################################
 
 
-resource "azurerm_linux_virtual_machine" "main_vm" {
-  name                = "JenkinsMaster"
-  resource_group_name = azurerm_resource_group.aks_rg.name
-  location            = azurerm_resource_group.aks_rg.location
-  size                = "Standard_D2s_v3"
-  disable_password_authentication = false
-  admin_username      = var.admin_username
-  admin_password = var.admin_password
-  network_interface_ids = [
-    azurerm_network_interface.vm_nic.id,
-  ]
+# resource "azurerm_linux_virtual_machine" "main_vm" {
+#   name                = "JenkinsMaster"
+#   resource_group_name = azurerm_resource_group.aks_rg.name
+#   location            = azurerm_resource_group.aks_rg.location
+#   size                = "Standard_D2s_v3"
+#   disable_password_authentication = false
+#   admin_username      = var.admin_username
+#   admin_password = var.admin_password
+#   network_interface_ids = [
+#     azurerm_network_interface.vm_nic.id,
+#   ]
 
   
-  custom_data = base64encode(
-    <<-CUSTOM_DATA
-    #!/bin/bash
-    sudo yum -y update
-    sudo yum -y install java-1.8.0-openjdk
-    sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-    sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-    sudo yum -y install jenkins
-    sudo systemctl start jenkins
-    CUSTOM_DATA
-  )
+#   custom_data = base64encode(
+#     <<-CUSTOM_DATA
+#     #!/bin/bash
+#     sudo yum -y update
+#     sudo yum -y install java-1.8.0-openjdk
+#     sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+#     sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+#     sudo yum -y install jenkins
+#     sudo systemctl start jenkins
+#     CUSTOM_DATA
+#   )
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
 
-  source_image_reference {
-    publisher = var.source_image_reference_publisher
-    offer     = var.source_image_reference_offer
-    sku       = var.source_image_reference_sku
-    version   = var.source_image_reference_version
-  }
+#   source_image_reference {
+#     publisher = var.source_image_reference_publisher
+#     offer     = var.source_image_reference_offer
+#     sku       = var.source_image_reference_sku
+#     version   = var.source_image_reference_version
+#   }
 
-}
+# }
 
-##########################################################################
+# ##########################################################################
